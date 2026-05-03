1d8638d734f484c8ee27416fa5a1b2e9388c0586
Abinash <abinash3101@yahoo.com>
2026-05-03 23:11:06 +0530

    LLM Hello world concepts

---
+++ LLM_Understanding/hello_world/gemini_openai.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key="AIzaSyATj86mzH99zTnG7MCeaMnRHyKApkqZgdE",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role":"user", "content":"Hay There I am Abinash. Who are you?"}
+    ]
+)
+
+print(response.choices[0].message.content)
